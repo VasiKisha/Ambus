f38068e8b610fff16df556fb520e8b327064d720
Vlastimil Krška <vlastimilkrska@seznam.cz>
2021-03-06 18:15:38 +0100

    cleaning code

--- AMBUS_Master/Echo.cs
+++ AMBUS_Master/Echo.cs
@@ -5,13 +5,7 @@
 //LICENSE file in the root directory of this source tree. 
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
